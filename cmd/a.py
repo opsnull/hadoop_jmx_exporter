39bf1cc5fb1e51f4b0f8f0232a69e2cf52e1e4b4
akenO8 <akeno8@163.com>
2023-09-19 12:03:28 +0800

    [ADD]add trino gc metrics

--- cmd/a.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-rsp = requests.get("http://pcdn.dq.baidu.com/v3/accesslog", stream=True)
-print (rsp.raw._connection.sock.getpeername()[0])
--- cmd/trino_coordinator.py
+++ cmd/trino_coordinator.py
@@ -62,6 +62,14 @@ class TrinoCoordinatorMetricCollector(MetricCollector):
                 self.setup_trino_coor_labels('ClusterMemoryManager')
             if 'trino.memory:type=ClusterMemoryPool,name=general' in beans[i]['objectName']:
                 self.setup_trino_coor_labels('ClusterMemoryPool')
+            if 'io.airlift.stats:name=GcMonitor' in beans[i]['objectName']:
+                self.setup_trino_coor_labels('GcMonitor')
+            if 'java.lang:name=G1 Young Generation,type=GarbageCollector' in beans[i]['objectName']:
+                # self.setup_trino_coor_labels('G1YoungGarbageCollector')
+                self.setup_young_gc_labels();
+            if 'java.lang:name=G1 Old Generation,type=GarbageCollector' in beans[i]['objectName']:
+                # self.setup_trino_coor_labels('G1OldGarbageCollector')
+                self.setup_old_gc_labels();
 
     def setup_trino_coor_labels(self, kind):
         label = ["cluster", "method", "_target"]
@@ -70,6 +78,50 @@ class TrinoCoordinatorMetricCollector(MetricCollector):
         # 暂时没有细分，如果后面在kind内部继续划分key，可以用上
         key = kind
         self.trino_coordinator_metrics[kind][key] = GaugeMetricFamily(name, description, labels=label)
+
+    def setup_young_gc_labels(self):
+        kind = 'G1YoungGarbageCollector'
+        label = ["cluster", "method", "_target"]
+        name = "_".join([self.trino_coordinator_prefix, kind])
+
+        young_before, young_after = 1, 1
+        for metric in self.metrics[kind]:
+            if metric.split('.')[0] == 'LastGcInfo':
+                key = metric.split('.')[1]
+                if key == 'memoryUsageBeforeGc' and young_before:
+                    description = 'Trino node memory usage metric before Young GC.'
+                    self.trino_coordinator_metrics[kind][key] = GaugeMetricFamily(name, description, labels=label)
+                    young_before = 0
+                elif key == 'memoryUsageAfterGc' and young_after:
+                    description = 'Trino node memory usage metric after Young GC.'
+                    self.trino_coordinator_metrics[kind][key] = GaugeMetricFamily(name, description, labels=label)
+                    young_after = 0
+            else:
+                key = kind
+                description = 'Trino node Old Young metric'
+                self.trino_coordinator_metrics[kind][key] = GaugeMetricFamily(name, description, labels=label)
+
+    def setup_old_gc_labels(self):
+        kind = 'G1OldGarbageCollector'
+        label = ["cluster", "method", "_target"]
+        name = "_".join([self.trino_coordinator_prefix, kind])
+
+        young_before, young_after = 1, 1
+        for metric in self.metrics[kind]:
+            if metric.split('.')[0] == 'LastGcInfo':
+                key = metric.split('.')[1]
+                if key == 'memoryUsageBeforeGc' and young_before:
+                    description = 'Trino node memory usage metric before Old GC.'
+                    self.trino_coordinator_metrics[kind][key] = GaugeMetricFamily(name, description, labels=label)
+                    young_before = 0
+                elif key == 'memoryUsageAfterGc' and young_after:
+                    description = 'Trino node memory usage metric after Old GC.'
+                    self.trino_coordinator_metrics[kind][key] = GaugeMetricFamily(name, description, labels=label)
+                    young_after = 0
+            else:
+                key = kind
+                description = 'Trino node Old GC metric'
+                self.trino_coordinator_metrics[kind][key] = GaugeMetricFamily(name, description, labels=label)
 
     def get_metrics(self, beans):
         for i in range(len(beans)):
@@ -87,13 +139,23 @@ class TrinoCoordinatorMetricCollector(MetricCollector):
                 self.get_trino_coor_labels(beans[i], 'ClusterMemoryManager')
             if 'trino.memory:type=ClusterMemoryPool,name=general' in beans[i]['objectName']:
                 self.get_trino_coor_labels(beans[i], 'ClusterMemoryPool')
+            if 'io.airlift.stats:name=GcMonitor' in beans[i]['objectName']:
+                self.get_trino_coor_labels(beans[i], 'GcMonitor')
+            if 'java.lang:name=G1 Young Generation,type=GarbageCollector' in beans[i]['objectName']:
+                # self.get_trino_coor_labels(beans[i], 'G1YoungGarbageCollector')
+                self.get_young_gc_labels(beans[i]);
+            if 'java.lang:name=G1 Old Generation,type=GarbageCollector' in beans[i]['objectName']:
+                # self.get_trino_coor_labels(beans[i], 'G1OldGarbageCollector')
+                self.get_old_gc_labels(beans[i]);
 
     def get_trino_coor_labels(self, bean, kind):
         # type(bean) = dict
         for metric in self.metrics[kind]:
+            key = kind
+            label = [self.cluster, '', self.target]
             value = 0
             for attr in bean['attributes']:
-                key = kind
+                # type(attr) = dict
                 method = metric.replace('.', '_').replace(':', '_').replace('-', '_')
                 label = [self.cluster, method, self.target]
                 if attr['name'] == metric:
@@ -103,4 +165,46 @@ class TrinoCoordinatorMetricCollector(MetricCollector):
                     else:
                         value = attr['value']
                     break
+            if not self.trino_coordinator_metrics[kind].has_key(key):
+                self.setup_trino_coor_labels(kind)
             self.trino_coordinator_metrics[kind][key].add_metric(label, value)
+
+    def get_young_gc_labels(self, bean):
+        kind = 'G1YoungGarbageCollector'
+        for metric in self.metrics[kind]:
+            method = metric.replace('.', '_').replace(':', '_').replace('-', '_')
+            label = [self.cluster, method, self.target]
+            value = 0
+            for attr in bean['attributes']:
+                if attr['name'] == 'LastGcInfo' and len(metric.split('.')) > 2 and metric.split('.')[0] == 'LastGcInfo':
+                    if 'value' in attr:
+                        key = metric.split('.')[1]
+                        for vl in attr['value'][key]:
+                            if vl['key'] == metric.split('.')[2]:
+                                manu = 'used'
+                                value = vl['value'][manu]
+                                self.trino_coordinator_metrics[kind][key].add_metric(label, value)
+                elif attr['name'] == metric:
+                    key = kind
+                    value = attr['value']
+                    self.trino_coordinator_metrics[kind][key].add_metric(label, value)
+
+    def get_old_gc_labels(self, bean):
+        kind = 'G1OldGarbageCollector'
+        for metric in self.metrics[kind]:
+            method = metric.replace('.', '_').replace(':', '_').replace('-', '_')
+            label = [self.cluster, method, self.target]
+            value = 0
+            for attr in bean['attributes']:
+                if attr['name'] == 'LastGcInfo' and len(metric.split('.')) > 2 and metric.split('.')[0] == 'LastGcInfo':
+                    if 'value' in attr:
+                        key = metric.split('.')[1]
+                        for vl in attr['value'][key]:
+                            if vl['key'] == metric.split('.')[2]:
+                                manu = 'used'
+                                value = vl['value'][manu]
+                                self.trino_coordinator_metrics[kind][key].add_metric(label, value)
+                elif attr['name'] == metric:
+                    key = kind
+                    value = attr['value']
+                    self.trino_coordinator_metrics[kind][key].add_metric(label, value)
